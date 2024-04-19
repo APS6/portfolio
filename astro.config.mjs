69f0d79115b2308f7710dccd91dca8f39d325226
APS <anirudhapsah@gmail.com>
2024-04-19 16:22:25 +0530

    output hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
