dd5d9a4f38c1c7022e598068ce6677b23660c771
Anirudha <me@anirudhasah.com>
2026-03-22 17:22:36 +0530

    static part done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Google Sans Code",
+      cssVariable: "--font-google",
+    },
+  ],
+});
